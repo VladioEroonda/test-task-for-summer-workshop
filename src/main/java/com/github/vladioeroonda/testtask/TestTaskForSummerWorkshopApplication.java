86e75f753f712377e08edc52c22d24705be5920a
VladioEroonda <vladioaf8@gmail.com>
2021-07-04 19:07:23 +0300

    Initial commit

---
+++ src/main/java/com/github/vladioeroonda/testtask/TestTaskForSummerWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.github.vladioeroonda.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForSummerWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskForSummerWorkshopApplication.class, args);
+    }
+
+}
